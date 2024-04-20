d8a57a37119a965061fa054722ee32f95a5e60e4
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-04-20 23:26:13 +0900

    Initial Commit

---
+++ SwiftUIDataFlow/SwiftUIDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDataFlowApp.swift
+//  SwiftUIDataFlow
+//
+//  Created by 조유진 on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
